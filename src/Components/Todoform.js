19f42edff33e28ba3cd68490a4edd8b69f27872b
Vrushali <varankarvrushali07@gmail.com>
2022-02-17 01:14:01 +0500

    This is final to do list app using react js

--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -18,11 +18,15 @@ export default function Todoform(props) {
         })
         setInput('')
     }
-
+    
     return ( 
+       <div>
+           <h1>What's your plan for today?</h1>
         <form className="todo-form">
+           
             <input type="text" placeholder="Add a ToDo" onChange={handleChange} className="todo-input" value={input} name="text" />
             <button type="submit" onClick={handleSubmit} className="todo-btn">ADD TODO</button>
         </form>
+        </div>
     );
 }
